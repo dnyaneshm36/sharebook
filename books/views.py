4119d926bed803d47b55eddace35d92e90bf271f
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-08 21:52:43 +0530

    ⚔⚔ final added. ⚔⚔

--- books/views.py
+++ books/views.py
@@ -6,5 +6,5 @@ from .models import Book
 from django.contrib import messages
 def Bookshows(request):
     books = Book.objects.all()
-    print(books)
+    # print(books)
     return render(request, "books.html", {'booksdis': books})
